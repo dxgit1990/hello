70084a250cb40e23aee88602c41033f5ae6adf4a
dx <283793790@qq.com>
2020-04-12 00:08:38 +0800

    add inner class and proxy model

---
+++ test/src/main/java/com/dx/proxy/ProxyTest.java
@@ -0,0 +1,12 @@
+package com.dx.proxy;
+
+/**
+ * @author dx
+ * @create 2020/4/12 0:07
+ * @for new
+ */
+public class ProxyTest {
+    public static void main(String[] args) {
+
+    }
+}
--- test/src/main/java/com/dx/test/Test3.java
+++ test/src/main/java/com/dx/test/Test3.java
@@ -7,8 +7,26 @@ package com.dx.test;
  */
 public class Test3 {
     public static void main(String[] args) {
-        test(1);
+//        test(1);
 //        test2(1);
+//        testHeapOverFlower();
+        testFor();
+    }
+
+    private static void testFor() {
+
+        for (int i = 0; i < 10; i++) {
+//            A a=new Test3().new A(i);
+            A a = new A(1);
+        }
+    }
+
+    private static void testHeapOverFlower() {
+//        for (; ; ) {
+        byte[] bytes=null;
+        while (true){
+            bytes = new byte[1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024];
+        }
     }
 
     public static void test(int start) {
@@ -24,4 +42,21 @@ public class Test3 {
             System.out.println(start);
         }
     }
+
+    public void testInnerClass(){
+        A a = new A(1);
+    }
+    static class A {
+        private int a;
+
+        public A(int a) {
+            this.a = a;
+        }
+        public int getA(){
+            return this.a;
+        }
+        public void finalize() throws Throwable{
+            System.out.println("对象被释放"+this.getA());
+        }
+    };
 }
